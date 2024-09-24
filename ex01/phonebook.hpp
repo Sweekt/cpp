1f9d1c366271f1ad076d8fbfa1c605bf9023ec27
beroy <benjamin.roy69@gmail.com>
2024-09-24 17:29:19 +0200

    initial push ex00-1

---
+++ ex01/phonebook.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 17:23:16 by beroy             #+#    #+#             */
+/*   Updated: 2024/09/24 17:24:55 by beroy            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include "contact.hpp"
+
+class	Phonebook
+{
+	private:
+		Contact	_contact[8];
+		int 	_index;
+	public:
+		Phonebook(void);
+		~Phonebook(void);
+		void	add(void);
+		void	search(void);
+		void	print(Contact contact);
+		Contact	get_contact(int index);
+};
+
+#endif
