709a0f54c8fff0c9d93b9b063730ce17f7c81845
beroy <benjamin.roy69@gmail.com>
2024-10-10 14:18:30 +0200

    weapon class added

--- 01/ex03/includes/Weapon.hpp
+++ 01/ex03/includes/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:01:53 by beroy             #+#    #+#             */
-/*   Updated: 2024/10/10 14:03:26 by beroy            ###   ########.fr       */
+/*   Updated: 2024/10/10 14:06:41 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@
 
 class Weapon {
 	private :
+		std::string	_type;
 	public :
 		Weapon();
 		~Weapon();
+		std::string	get_type(void) const;
+		void		set_type(std::string str);
 };
 
 #endif
--- 01/ex03/srcs/Weapon.cpp
+++ 01/ex03/srcs/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:02:12 by beroy             #+#    #+#             */
-/*   Updated: 2024/10/10 14:02:12 by beroy            ###   ########.fr       */
+/*   Updated: 2024/10/10 14:03:26 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,12 @@ Weapon::Weapon(void) {
 }
 
 Weapon::~Weapon(void) {
+}
+
+std::string	Weapon::get_type(void) const {
+	return (this->_type);
+}
+
+void	Weapon::set_type(std::string str) {
+	this->_type = str;
 }
