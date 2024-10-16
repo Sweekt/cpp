ab9b945afc6645a332137eee3fcd27f681efb28e
beroy <benjamin.roy69@gmail.com>
2024-10-16 20:28:24 +0200

    started 03

---
+++ 03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,17 @@
+//
+// Created by beroy on 10/16/24.
+//
+
+#ifndef CPP_CLAPTRAP_HPP
+# define CPP_CLAPTRAP_HPP
+
+class ClapTrap
+{
+private :
+public :
+	ClapTrap();
+
+	~ClapTrap();
+};
+
+#endif //CPP_CLAPTRAP_HPP
---
+++ 03/ex00/srcs/ClapTrap.cpp
@@ -0,0 +1,13 @@
+//
+// Created by beroy on 10/16/24.
+//
+
+#include "../includes/ClapTrap.hpp"
+
+ClapTrap::ClapTrap(void)
+{
+}
+
+ClapTrap::~ClapTrap(void)
+{
+}
