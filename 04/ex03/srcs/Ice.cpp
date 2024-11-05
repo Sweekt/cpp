583fa9317db7d86e3ebb2f4f0493b6945289db5c
beroy <benjamin.roy69@gmail.com>
2024-11-05 15:55:35 +0100

    added ice

--- 04/ex03/srcs/Ice.cpp
+++ 04/ex03/srcs/Ice.cpp
@@ -6,14 +6,35 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:47:21 by beroy             #+#    #+#             */
-/*   Updated: 2024/11/05 11:47:21 by beroy            ###   ########.fr       */
+/*   Updated: 2024/11/05 14:32:11 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Ice.hpp"
 
-Ice::Ice(void) {
+Ice::Ice(void) : AMateria("ice") {
+	std::cout << "Ice materia default constructor called." << std::endl;
+}
+Ice::Ice(const Ice &copy) {
+	std::cout << "Ice materia copy constructor called." << std::endl;
+	*this = copy;
 }
 
 Ice::~Ice(void) {
+	std::cout << "Ice default destructor called." << std::endl;
+}
+
+Ice	&Ice::operator=(const Ice *src) {
+	if (this != src)
+		this->_type = src->_type;
+	return (*this);
+}
+
+void	Ice::use(ICharacter &target) {
+	std::cout << "* shoots an ice bolt at " << target.getname << " *" << std::endl;
+}
+
+Ice		*Ice::clone(void) const {
+	Ice	*clone = new Ice;
+	return (clone);
 }
