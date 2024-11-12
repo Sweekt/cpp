161ae46364f36bbe9652a1bc60aac9078f64ce6a
beroy <benjamin.roy69@gmail.com>
2024-11-12 16:00:48 +0100

    Updated 04/01

--- 05/ex00/includes/Bureaucrat.hpp
+++ 05/ex00/includes/Bureaucrat.hpp
@@ -6,26 +6,57 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:40:09 by beroy             #+#    #+#             */
-/*   Updated: 2024/11/07 15:50:35 by beroy            ###   ########.fr       */
+/*   Updated: 2024/11/12 15:44:48 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+# include <iostream>
+
 class Bureaucrat
 {
 private :
-	int	_grade;
+	const std::string	_name;
+	int					_grade;
 public :
+	// Constructor & destructors
+
 	Bureaucrat();
-	Bureaucrat(int grade);
-	Bureaucrat(Bureaucrat &copy);
+	Bureaucrat(std::string name, int grade);
+	Bureaucrat(const Bureaucrat &copy);
 	~Bureaucrat();
 
-	Bureaucrat	&operator=(Bureaucrat &src);
+	// Overloaded operators
 
+	Bureaucrat	&operator=(const Bureaucrat &src);
 
+	// Public methods
+
+	void	gradeUp();
+	void	gradeDown();
+
+	// Setters & getters
+
+	int			get_grade(void) const;
+	std::string	get_name(void) const;
+	void		set_grade(int grade);
+
+	// Exceptions
+	class GradeTooLowException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char *what() const throw();
+	};
 };
+
+// Iostream overload
+
+std::ostream 	&operator<<(std::ostream &out, const Bureaucrat &src);
 
 #endif
--- 05/ex00/srcs/main.cpp
+++ 05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:44:27 by beroy             #+#    #+#             */
-/*   Updated: 2024/11/07 15:44:27 by beroy            ###   ########.fr       */
+/*   Updated: 2024/11/12 15:58:48 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,27 @@
 
 int	main()
 {
-	return (0);
+	Bureaucrat bob("Bob", 1);
+	Bureaucrat bob2(bob);
+	std::cout << bob;
+	std::cout << bob2;
+	try
+	{
+		bob.gradeUp();
+		std::cout << "Test; this should not be printed" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << "Incrementing grade of " << bob.get_name() << " failed: " << e.what() << std::endl;
+	}
+	std::cout << bob;
+	try
+	{
+		Bureaucrat mich("Michel", 151);
+		std::cout << "Test; this should not be printed" << std::endl;
+	}
+	catch (Bureaucrat::GradeTooLowException &e)
+	{
+		std::cerr << "Creation of grade 151 bureau has failed : " << e.what() << std::endl;
+	}
 }
