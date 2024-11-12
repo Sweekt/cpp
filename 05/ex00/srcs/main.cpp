e37ee7d2997d9e32300fae4c4495b8e7746919a7
beroy <benjamin.roy69@gmail.com>
2024-11-12 16:30:12 +0100

    Updated 04/01

--- 05/ex00/srcs/main.cpp
+++ 05/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 15:44:27 by beroy             #+#    #+#             */
-/*   Updated: 2024/11/12 15:58:48 by beroy            ###   ########.fr       */
+/*   Updated: 2024/11/12 15:58:59 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,18 @@ int	main()
 	catch (Bureaucrat::GradeTooLowException &e)
 	{
 		std::cerr << "Creation of grade 151 bureau has failed : " << e.what() << std::endl;
+	}
+	try
+	{
+		bob2.gradeDown();
+		std::cout << "Test; this should be printed." << std::endl;
+		bob2.gradeUp();
+		std::cout << "Test; this should be printed." << std::endl;
+		bob2.gradeUp();
+		std::cout << "Test; this shouldn't be printed." << std::endl;
+	}
+	catch (Bureaucrat::GradeTooHighException &e)
+	{
+		std::cerr << "Incrementing grade of " << bob2.get_name() << " failed: " << e.what() << std::endl;
 	}
 }
