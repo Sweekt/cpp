01b75870bc4a0097a258a3e9972543d4c4a96dfd
beroy <benjamin.roy69@gmail.com>
2025-01-14 12:41:27 +0100

    Error now output on cerr

--- 09/ex01/srcs/main.cpp
+++ 09/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:42:08 by beroy             #+#    #+#             */
-/*   Updated: 2025/01/13 19:53:27 by beroy            ###   ########.fr       */
+/*   Updated: 2025/01/14 12:39:14 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,51 @@
 int main(int ac, char **av) {
 	RPN	polish;
 	if (ac != 2) {
-		std::cout << "Wrong input! Run with ./RPN \"<expression>\"" << std::endl;
+		std::cerr << "Wrong input! Run with ./RPN \"<expression>\"" << std::endl;
 		return (0);
 	}
+	std::cout << "---------------Input tests:-------------" << std::endl;
 	try {
+		std::cout << "Input: " << av[1] << std::endl;
 		polish.calculator(av[1]);
 		std::cout << "Result: " << polish.get_result() << std::endl;
 	}
 	catch (std::exception &e) {
-		std::cout << "Error: " << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
 	}
+//	std::cout << "---------------More tests:--------------" << std::endl;
+//	try {
+//		std::cout << "Test 1: " << "123++" << std::endl;
+//		polish.calculator("123++");
+//		std::cout << "Result: " << polish.get_result() << std::endl;
+//	}
+//	catch (std::exception &e) {
+//		std::cerr << "Error: " << e.what() << std::endl;
+//	}
+//	try {
+//		std::cout << "Test 2: " << "123**5+98/-" << std::endl;
+//		polish.calculator("123**5+98/-");
+//		std::cout << "Result: " << polish.get_result() << std::endl;
+//	}
+//	catch (std::exception &e) {
+//		std::cerr << "Error: " << e.what() << std::endl;
+//	}
+//	try {
+//		std::cout << "Test 3: " << "1234/" << std::endl;
+//		polish.calculator("1234/");
+//		std::cout << "Result: " << polish.get_result() << std::endl;
+//	}
+//	catch (std::exception &e) {
+//		std::cerr << "Error: " << e.what() << std::endl;
+//	}
+//	try {
+//		std::cout << "Test 5: " << "++45-88" << std::endl;
+//		polish.calculator("++45-88");
+//		std::cout << "Result: " << polish.get_result() << std::endl;
+//	}
+//	catch (std::exception &e) {
+//		std::cerr << "Error: " << e.what() << std::endl;
+//	}
+//	std::cout << "----------------------------------------" << std::endl;
+	return (0);
 }
