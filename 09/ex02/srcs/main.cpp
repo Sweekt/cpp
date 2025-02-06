d704d12000340f69846b00a389625dda7789908e
beroy <benjamin.roy69@gmail.com>
2025-02-06 13:13:25 +0100

    Parsing of parameters added

--- 09/ex02/srcs/main.cpp
+++ 09/ex02/srcs/main.cpp
@@ -6,27 +6,53 @@
 /*   By: beroy <beroy@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 08:34:41 by beroy             #+#    #+#             */
-/*   Updated: 2025/02/03 13:27:56 by beroy            ###   ########.fr       */
+/*   Updated: 2025/02/06 13:08:40 by beroy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/PMergeMe.hpp"
 
-std::vector<int>	fill_vec(int ac, char **av)
-{
-	std::vector<int>	vec;
+template <typename T>
+void	fillArray(T *array, int ac, char **av) {
 	for (int i = 1; i < ac; i++) {
-		vec.push_back(atol(av[i]));
+		array->push_back(atol(av[i]));
 	}
-	return (vec);
+}
+
+bool isNumber(const std::string &str) {
+	bool	sign = false;
+	for (std::string::const_iterator it = str.begin(); it != str.end(); it++) {
+		if (!sign && (*it == '-' || *it == '+'))
+			sign = true;
+		else if (sign && (*it == '-' || *it == '+'))
+			return (false);
+		else if (!std::isdigit(*it))
+			return (false);
+	}
+	return (true);
+}
+
+bool checkParams(int ac, char** av) {
+	for (int i = 1; i < ac; i++) {
+		if (!isNumber(av[i])) {
+			return (false);
+		}
+	}
+	return (true);
 }
 
 int main(int ac, char **av) {
 	std::vector<int>	vec;
 
-	if (ac < 2)
+	if (ac <= 2) {
+		std::cout << "Add more params; At least two." << std::endl;
 		return (0);
-	vec = fill_vec(ac, av);
+	}
+	if (!checkParams(ac, av)) {
+		std::cout << "Params should only be numbers!" << std::endl;
+		return (0);
+	}
+	fillArray(&vec, ac, av);
 	PMergeMe::sortVec(vec);
 	return (0);
 }
